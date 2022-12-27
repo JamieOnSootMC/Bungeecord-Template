30e6372d3ae7a8ccfc0f0bcf71bedfa26f20bad2
JamieOnSootMC <jamie@sootmc.net>
2022-12-27 15:14:08 +0000

    Initial commit

---
+++ src/main/java/net/sootmc/exampleplugin/ExamplePlugin.java
@@ -0,0 +1,16 @@
+package net.sootmc.exampleplugin;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class ExamplePlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
